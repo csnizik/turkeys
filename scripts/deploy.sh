2f2ed93619f0992354a76a8ea468ed4cdf9bfc24
Forsyth Bacon <Forsyth.Bacon@usda.gov>
2024-02-06 18:23:43 -0600

    Pull request #67: CIGCPDPH2 UpdateScripts
    Merge in NRCSAPPS/cpdifrontend from CIGCPDPH2-UpdateScripts to develop
    
    Squashed commit of the following:
    
    commit 8eeefd08b06ddeb8d538f8aa4e2af02e027e8391
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Tue Feb 6 11:19:20 2024 -0800
    
        Fixes after testing
    
    commit 2343febd4259c2fd861e71e79a46ac6b9f81e449
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Tue Feb 6 09:11:35 2024 -0800
    
        Corrections
    
    commit 33763257ff694f03da9f73c3f4ada73e4bd1d3de
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Mon Feb 5 15:51:55 2024 -0800
    
        unverbosed untarring
    
    commit dbc9b5b850fcbabc1b5bcc168d2e3329bf72d2a4
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Mon Feb 5 15:42:33 2024 -0800
    
        Fixed tarball file name issue for development
    
    commit b656273136870d50aa6a8ffdea736853f8badd19
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Mon Feb 5 15:30:21 2024 -0800
    
        New script for all envs based on dev script

---
+++ scripts/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# place in the /app/scripts folder with execute permission for all
+# executed from users home folder as the user, where they copied the tarball to
+
+SUPPORTED_ENVS='dev test uat stage prod'
+if [ $# -lt 2 ]; then
+        echo "Usage - <tarball_file_version_number> <environment (dev/test/uat/stage/prod)>"
+        exit 1
+fi
+
+declare -A envs=( ["dev"]="cpdi-dev.dev" ["test"]="cpdi-test.cert" ["uat"]="cpdi-uat.cert" ["stage"]="cpdi-stage.cert" ["prod"]="cpdi" )
+
+#Lookup domain
+DOMAIN="${envs[$2]}"
+if [ $1 = "deploy" ]; then
+  DEPLOY_FILE="cpdifrontend.tar.gz"
+else
+  DEPLOY_FILE="cpdifrontend.$1.tar.gz"
+fi
+
+
+echo "moving tar file  $1 to /app/tmp"
+mv -f $DEPLOY_FILE /app/tmp
+
+echo "stopping the service"
+systemctl stop $DOMAIN.service
+systemctl status $DOMAIN.service
+echo "changing to /app/www/html folder"
+cd /app/www/html
+echo "removing all files in the $DOMAIN folder"
+rm -rf $DOMAIN/*
+echo "changing to /app/www/html/$DOMAIN folder"
+cd /app/www/html/$DOMAIN
+echo "explode tarball"
+tar -xf /app/tmp/$DEPLOY_FILE
+echo "Setting API base URL"
+APIDOMAIN="${DOMAIN/-/api-}"
+echo "Setting API base URL to https://$APIDOMAIN.sc.egov.usda.gov"
+grep -Rl 'https://cpdiapi-dev.dev.sc.egov.usda.gov/' . | xargs sed -i  "s_https://cpdiapi-dev.dev.sc.egov.usda.gov/_https://$APIDOMAIN.sc.egov.usda.gov/_g"
+echo "change owner of files to appadmin"
+cd ../
+chown -R appadmin:appadmin $DOMAIN
+echo "start the service"
+systemctl start $DOMAIN.service
+
+echo "done"
--- scripts/s1_deploy.sh
+++ scripts/s1_deploy.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-# place in the /app/scripts folder with execute permission for all
-# executed from users home folder as the user, where they copied the tarball to
-echo "moving tar file to /app/tmp"
-mv -f cpdifrontend.tar.gz /app/tmp
-echo "changing to /app/www/html/cpdi-dev.dev folder"
-cd /app/www/html/cpdi-dev.dev
-echo "stopping the service"
-systemctl stop cpdi-dev.dev.service
-systemctl status cpdi-dev.dev.service
-cd /app/www/html/cpdi-dev.dev
+# This script was copied into deploy.sh and modified to be suitable for all envs.
+# This script now calls deploy.sh supplying the default arguments as a convenience mechanism.
 
-if ! [[ -d "../cpdi-dev.dev" ]]
-then
-    echo "Run this from inside /app/www/html/cpdi-dev.dev."
-    exit 1
-fi
-
-echo "removing all files in this folder"
-rm -rf *
-echo "explode tarball"
-tar -xf /app/tmp/cpdifrontend.tar.gz
-echo "change owner of files to appadmin"
-cd ../
-chown -R appadmin:appadmin cpdi-dev.dev
-echo "start the service"
-systemctl start cpdi-dev.dev.service
-
-echo done
+/app/scripts/deploy.sh deploy dev
